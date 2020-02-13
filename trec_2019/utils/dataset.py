460f949e41f038066eda3b6d902e649494450224
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-02-13 22:21:40 +0900

    TRECTripleEmbeddingDataset

--- trec_2019/utils/dataset.py
+++ trec_2019/utils/dataset.py
@@ -2,47 +2,55 @@ import torch
 import pandas as pd
 import gzip
 from torch.utils.data import Dataset, ConcatDataset, IterableDataset
+import h5py
+import numpy as np
 
 
 class TRECTripleEmbeddingDataset(Dataset):
-    HDF_KEY = '/data'
-    IDX_UID = 1
-    IDX_EMB = 2
+    HDF_KEY = "/data"
+    IDX_Q_EMB = 0
+    IDX_POS_EMB = 1
+    IDX_NEG_EMB = 2
 
     def __init__(self, data_path):
         super().__init__()
         self.data_path = data_path
         self.data = None
-        self._data_shape = self._read_data_shape()
+        self._num_samples = self._read_num_samples()
+        self._dimension = self._read_dimension()
 
-    def _read_data_shape(self):
+    def get_dimension(self):
+        return self._dimension
+
+    def _read_dimension(self):
         with h5py.File(self.data_path, "r") as fin:
             dataset = fin[self.HDF_KEY]["table"]
-            num_samples = dataset.shape[0]
-            dimension = dataset[0][self.vector_idx].shape[0]
-            return (num_samples, dimension)
+            return dataset[0][0].shape[0]
 
-    def get_data_shape(self):
-        return self._data_shape
+    def _read_num_samples(self):
+        with h5py.File(self.data_path, "r") as fin:
+            dataset = fin[self.HDF_KEY]["table"]
+            return dataset.shape[0]
 
     def __len__(self):
-        return self._data_shape[0]
+        return self._num_samples
 
     def __getitem__(self, index):
         if self.data is None:
             self.data = h5py.File(self.data_path, "r")[self.HDF_KEY]["table"]
+        # return a sample
         sample = self.data[index]
-        uid = sample[self.IDX_UID][0].decode(encoding="utf-8")
-        vector = np.array(sample[self.IDX_EMB], dtype=np.float32)
-        return {"id": uid, "vector": vector}
+        q_emb = np.array(sample[self.IDX_Q_EMB], dtype=np.float32)
+        pos_emb = np.array(sample[self.IDX_POS_EMB], dtype=np.float32)
+        neg_emb = np.array(sample[self.IDX_NEG_EMB], dtype=np.float32)
+        return {"q_emb": q_emb, "pos_emb": pos_emb, "neg_emb": neg_emb}
 
 
 class TRECTripleIdDataset(Dataset):
     def __init__(self, data_path):
         super().__init__()
         self.data_path = data_path
-        self.data = pd.read_csv(data_path, sep='\t', names=[
-                                'q_id', 'pos_id', 'neg_id'])
+        self.data = pd.read_csv(data_path, sep="\t", names=["q_id", "pos_id", "neg_id"])
 
     def __len__(self):
         return len(self.data)
