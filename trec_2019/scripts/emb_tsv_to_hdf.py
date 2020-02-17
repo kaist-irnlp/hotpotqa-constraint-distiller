0797c74258875186a2665808f5a94be55fe9ef17
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-02-17 18:03:08 +0900

    trivial

--- trec_2019/scripts/emb_tsv_to_hdf.py
+++ trec_2019/scripts/emb_tsv_to_hdf.py
@@ -16,6 +16,7 @@ logger = logging.getLogger(__name__)
 parser = ArgumentParser()
 parser.add_argument("--data_path", type=str, required=True)
 parser.add_argument("--out_path", type=str, default=None)
+parser.add_argument("--low_memory", action="store_true")
 args = parser.parse_args()
 
 COMP_LEVEL = 9
@@ -24,7 +25,7 @@ fields = ["id", "vector"]
 
 
 def parse_id_and_vector(tsv_line):
-    uid, vector = tsv_line.split('\t')
+    uid, vector = tsv_line.split("\t")
     uid = int(uid)
     vector = np.array(vector.split(), np.float32)
     return (uid, *vector)
@@ -34,7 +35,9 @@ def make_meta(data_path):
     with open(data_path, encoding="utf-8") as f:
         line = f.readline()
         uid, *vector = parse_id_and_vector(line)
-        return [("id", np.int32)] + [(f"d{i+1}", np.float32) for i, val in enumerate(vector)]
+        return [("id", np.int32)] + [
+            (f"d{i+1}", np.float32) for i, val in enumerate(vector)
+        ]
 
 
 if __name__ == "__main__":
@@ -42,18 +45,20 @@ if __name__ == "__main__":
         # read
         data_path = Path(args.data_path)
         meta = make_meta(data_path)
-        d = db.read_text(data_path).map(
-            parse_id_and_vector).to_dataframe(meta).drop_duplicates(subset=["id"]).set_index("id")
+        d = db.read_text(data_path).map(parse_id_and_vector).to_dataframe(meta=meta)
+        # .drop_duplicates(subset=["id"]).set_index("id")
         num_samples = len(d)
 
         # SAVE
-        out_path = data_path.parent / \
-            f"{data_path.stem}.hdf5" if not args.out_path else args.out_path
+        fname = (
+            Path(data_path.stem) / f"{data_path.stem}.*.hdf5" if args.low_memory else f"{data_path.stem}.hdf5"
+        )
+        out_path = data_path.parent / fname if not args.out_path else args.out_path
         data_columns = ["id"]
         d.to_hdf(
             out_path,
             HDF_PATH,
             mode="w",
             data_columns=data_columns,
-            complevel=COMP_LEVEL
+            complevel=COMP_LEVEL,
         )
