7ba3f2a1d8459f8714e4dbf69ca3c3c805cec125
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-02-22 00:13:01 +0900

    trivial

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@
 
 from setuptools import setup, find_packages
 
-setup(name='trec_2019',
+setup(name='trec2019',
       version='0.0.1',
       description='TREC 2019 Deep Learning',
       author='Kyoung-Rok Jang',
       author_email='kyoungrok.jang@gmail.com',
       url='https://github.com/kyoungrok0517/trec-2019-deep-learning',
       install_requires=[
-            'pytorch-lightning'
+            'pytorch-lightning',
+            'torch',
+            'transformers',
+            'h5py',
+            'tqdm',
+            'pyarrow',
+            'python-snappy'
       ],
       packages=find_packages()
       )
---
+++ trec2019/utils/dataset.py
@@ -0,0 +1,59 @@
+import torch
+import pandas as pd
+import gzip
+from torch.utils.data import Dataset, ConcatDataset, IterableDataset
+import h5py
+import numpy as np
+from trec2019.utils.encoder import BertEncoder
+
+
+class TRECTripleDataset(Dataset):
+    def __init__(self, data_path):
+        super().__init__()
+        self.data = pd.read_parquet(data_path)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        # return a sample
+        tr = self.data.iloc[index]
+        return {
+            "query": tr["query"],
+            "doc_pos": tr["doc_pos"],
+            "doc_neg": tr["doc_neg"],
+        }
+
+
+class TRECTripleEmbeddingDataset(TRECTripleDataset):
+    def __init__(self, data_path, encoder):
+        super().__init__(data_path)
+        self.encoder = encoder
+
+    def __getitem__(self, index):
+        # return a sample
+        sample = super().__getitem__(index)
+        for fld in ["query", "doc_pos", "doc_neg"]:
+            sample[fld] = self.encoder.encode(sample[fld])
+        return sample
+
+
+class TRECTripleIdDataset(Dataset):
+    def __init__(self, data_path):
+        super().__init__()
+        self.data_path = data_path
+        self.data = pd.read_csv(data_path, sep="\t", names=["q_id", "pos_id", "neg_id"])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data.iloc[index].to_dict()
+
+if __name__ == '__main__':
+    encoder = BertEncoder()
+    fpath = '/Users/kyoungrok/Resilio Sync/Dataset/2019 TREC/passage_ranking/triples.train.tiny.parquet'
+    dset = TRECTripleEmbeddingDataset(fpath, encoder)
+    for d in dset:
+        print(d)
+        break
