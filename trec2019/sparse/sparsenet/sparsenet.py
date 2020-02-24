b5f4d3a02425be43fcf27e2856ebd69933a664bc
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-02-24 15:28:39 +0900

    trivial

--- trec2019/sparse/sparsenet/sparsenet.py
+++ trec2019/sparse/sparsenet/sparsenet.py
@@ -4,6 +4,7 @@ This file defines the core research contribution
 import os
 import torch
 from torch import optim
+from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
@@ -20,9 +21,10 @@ from torch.utils.data.distributed import DistributedSampler
 
 from trec2019.utils.dataset import TRECTripleBERTDataset
 from trec2019.utils.encoder import BertEncoder
+from trec2019.sparse.sparsenet.helper import *
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+   format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
@@ -32,16 +34,164 @@ class SparseNet(pl.LightningModule):
     def __init__(self, hparams):
         super(SparseNet, self).__init__()
         self.hparams = hparams
+        self._encoded = None
         self._load_dataset()
+        # network
+        self._validate_network_params()
+        self._init_network()
 
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.linear_sdr(x)
+        x = self.fc(x)
+
+        if self.training:
+            batch_size = x.shape[0]
+            self.learning_iterations += batch_size
+        
+        return x
+
+    def _init_network(self):
+        self.learning_iterations = 0
+        self.flatten = Flatten()
+
+        # Linear layers only (from original code)
+        input_features = self.hparams.input_size
+        output_size = self.hparams.output_size
+        n = self.hparams.n
+        k = self.hparams.k
+        normalize_weights = self.hparams.normalize_weights
+        weight_sparsity = self.hparams.weight_sparsity
+        use_batch_norm = self.hparams.use_batch_norm
+        dropout = self.hparams.dropout
+        k_inference_factor = self.hparams.k_inference_factor
+        boost_strength = self.hparams.boost_strength
+        boost_strength_factor = self.hparams.boost_strength_factor
+
+        self.linear_sdr = nn.Sequential()
+        for i, n_i in enumerate(n):
+            if n_i != 0:
+                linear = nn.Linear(input_features)
+                if 0 < weight_sparsity[i] < 1:
+                    linear = SparseWeights(linear, weightSparsity=weight_sparsity[i])
+                    if normalize_weights:
+                        linear.apply(normalizeSparseWeights)
+                self.linear_sdr.add_module(f"linear_sdr{i+1}", linear)
+
+                if use_batch_norm:
+                    self.linear_sdr.add_module(
+                        f"linear_sdr{i+1}_bn", nn.BatchNorm1d(n_i, affine=False)
+                    )
+                
+                if dropout > 0.0:
+                    self.linear_sdr.add_module(
+                        f"linear_sdr{i+1}_dropout", nn.Dropout(dropout)
+                    )
+
+                if 0 < k[i] < n[i]:
+                    kwinner = KWinners(
+                        n=n[i],
+                        k=k[i],
+                        kInferenceFactor=k_inference_factor,
+                        boostStrength=boost_strength,
+                        boostStrengthFactor=boost_strength_factor,
+                    )
+                    self.linear_sdr.add_module(
+                        f"linear_sdr{i+1}_kwinner", kwinner
+                    )
+                else:
+                    self.linear_sdr.add_module(
+                        f"linear_sdr{i+1}_relu", nn.ReLU()
+                    )
+                # Feed this layer output into next layer input
+                input_features = n_i
+
+        # Add one fully connected layer after all hidden layers
+        self.fc = nn.Linear(input_features, output_size)
+
+    def postEpoch(self):
+        self.apply(updateBoostStrength)
+        self.apply(rezeroWeights)
+    
+    def get_encoded(self):
+        return self._encoded
+
+    def get_learning_iterations(self):
+        return self.learning_iterations
+
+    def maxEntropy(self):
+        entropy = 0
+        for module in self.modules():
+            if module == self:
+                continue
+            if hasattr(module, "maxEntropy"):
+                entropy += module.maxEntropy()
+        return entropy
+
+    def entropy(self):
+        entropy = 0
+        for module in self.modules():
+            if module == self:
+                continue
+            if hasattr(module, "entropy"):
+                entropy += module.entropy()
+        return entropy
+
+    def pruneWeights(self, minWeight):
+        """
+        Prune all the weights whose absolute magnitude is less than minWeight
+        :param minWeight: min weight to prune. If zero then no pruning
+        :type minWeight: float
+        """
+        if minWeight == 0.0:
+            return
+
+        # Collect all weights
+        weights = [v for k, v in self.named_parameters() if "weight" in k]
+        for w in weights:
+            # Filter weights above threshold
+            mask = torch.ge(torch.abs(w.data), minWeight)
+            # Zero other weights
+            w.data.mul_(mask.type(torch.float32))
+
+    def pruneDutycycles(self, threshold=0.0):
+        """
+        Prune all the units with dutycycles whose absolute magnitude is less than
+        the given threshold
+        :param threshold: min threshold to prune. If less than zero then no pruning
+        :type threshold: float
+        """ 
+        if threshold < 0.0:
+            return
+
+        # Collect all layers with 'dutyCycle'
+        for m in self.modules():
+            if m == self:
+                continue
+            if hasattr(m, "pruneDutycycles"):
+                m.pruneDutycycles(threshold)
+
+    def _validate_network_params(self):
+        hparams = self.hparams
+        if type(hparams.n) is not list:
+            hparams.n = [hparams.n]
+        if type(hparams.k) is not list:
+            hparams.k = [hparams.k] * len(hparams.n)
+        assert len(hparams.n) == len(hparams.k)
+        for i in range(len(hparams.n)):
+            assert hparams.k[i] <= hparams.n[i]
+        if type(hparams.weight_sparsity) is not list:
+            hparams.weight_sparsity = [hparams.weight_sparsity] * len(hparams.n)
+        assert len(hparams.n) == len(hparams.weight_sparsity)
+        for i in range(len(hparams.weight_sparsity)):
+            assert hparams.weight_sparsity[i] >= 0
+        
     def _load_dataset(self):
         data_dir = Path(self.hparams.data_dir)
         self._train_dataset = TRECTripleBERTDataset(data_dir / "train.parquet")
         self._val_dataset = TRECTripleBERTDataset(data_dir / "valid.parquet")
         self._test_dataset = TRECTripleBERTDataset(data_dir / "test.parquet")
 
-    def forward(self, x):
-        return torch.relu(self.l1(x.view(x.size(0), -1)))
 
     def training_step(self, batch, batch_idx):
         # REQUIRED
