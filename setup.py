69825e1485871e1c16c00dddcd6baa5510a40466
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-03-06 16:37:48 +0900

    trivial

--- setup.py
+++ setup.py
@@ -2,22 +2,24 @@
 
 from setuptools import setup, find_packages
 
-setup(name='trec2019',
-      version='0.0.1',
-      description='TREC 2019 Deep Learning',
-      author='Kyoung-Rok Jang',
-      author_email='kyoungrok.jang@gmail.com',
-      url='https://github.com/kyoungrok0517/trec-2019-deep-learning',
-      install_requires=[
-            'pytorch-lightning',
-            'torch',
-            'transformers',
-            'h5py',
-            'tqdm',
-            'pyarrow',
-            'python-snappy',
-            'matplotlib'
-      ],
-      packages=find_packages()
-      )
+setup(
+    name="trec2019",
+    version="0.0.1",
+    description="TREC 2019 Deep Learning",
+    author="Kyoung-Rok Jang",
+    author_email="kyoungrok.jang@gmail.com",
+    url="https://github.com/kyoungrok0517/trec-2019-deep-learning",
+    install_requires=[
+        "pytorch-lightning",
+        "torch",
+        "transformers",
+        "h5py",
+        "tqdm",
+        "pyarrow",
+        "python-snappy",
+        "matplotlib",
+        "gensim",
+    ],
+    packages=find_packages(),
+)
 
