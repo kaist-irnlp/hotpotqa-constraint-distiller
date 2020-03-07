147628213d20268f27c1510cd137b9ff7a31fcb3
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-03-07 22:56:20 +0900

    trivial

--- trec2019/model/sparsenet/sparsenet.py
+++ trec2019/model/sparsenet/sparsenet.py
@@ -7,6 +7,7 @@ import torch
 import gc
 from torch import optim
 from torch import nn
+from torch import tensor
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
@@ -28,6 +29,7 @@ from transformers import BertModel
 from transformers import BertTokenizer
 import gensim
 from gensim.models.keyedvectors import KeyedVectors
+import numpy as np
 
 from trec2019.utils.dataset import (
     TRECTripleBERTDataset,
@@ -61,6 +63,7 @@ class DiscEmbedding(nn.Module):
 
     def forward(self, batch_text):
         batch_embeddings = [self._embed(text) for text in batch_text]
+        return batch_embeddings
         # with self.nlp.disable_pipes("tagger", "parser"):
         #     docs = self.nlp.pipe(batch_text)
         # batch_token_ids = [[self.word2idx(w) for w in doc] for doc in docs]
@@ -68,9 +71,20 @@ class DiscEmbedding(nn.Module):
         # embeddings = self.embeddings(docs).view(1, -1)
 
     def _embed(self, text):
-        blob = TextBlob(text)
+        blob = TextBlob(text).lower()
+        out_dim = self.emb_dim * self.ngram
+        out = torch.zeros(out_dim)
         for n in range(1, self.ngram + 1):
             ngrams = blob.ngrams(n=n)
+            ngrams_emb = torch.zeros(self.emb_dim)
+            for i, ng in enumerate(ngrams):
+                ng_ids = [self.word2idx.get(w, -1) for w in ng]
+                ng_ids = tensor([i for i in ng_ids if i != -1], dtype=torch.long)
+                ng_embs = self.embeddings(ng_ids)
+                ngrams_emb += torch.prod(ng_embs, 0)
+            # out[ngram_range] = ngrams_emb
+            out[self.emb_dim * (n - 1) : self.emb_dim * n] = ngrams_emb / (i + 1)
+        return out
 
     def _init_tokenizer(self):
         self.nlp = spacy.load("en")
@@ -80,8 +94,10 @@ class DiscEmbedding(nn.Module):
         model = KeyedVectors.load(self.embedding_path)
         # save
         self.embeddings = nn.Embedding.from_pretrained(
-            torch.tensor(model.vectors, dtype=torch.float32), freeze=True
+            tensor(model.vectors, dtype=torch.float32), freeze=True
         )
+        self.vocab_size = self.embeddings.num_embeddings
+        self.emb_dim = self.embeddings.embedding_dim
         self.word2idx = {w: idx for (idx, w) in enumerate(model.index2word)}
         # clean
         del model
