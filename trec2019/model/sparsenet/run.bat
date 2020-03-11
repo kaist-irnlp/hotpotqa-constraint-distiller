ba919eeb52ecb14dc260d24bd2f295915e6aa276
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-03-11 19:52:29 +0900

    trivial

--- trec2019/model/sparsenet/run.bat
+++ trec2019/model/sparsenet/run.bat
@@ -1,1 +1,6 @@
-python sparsenet_trainer.py --data_dir ../../data -e ../../data/embedding/glove.840B.300d.gensim --gpus 0 --use_amp
+python sparsenet_trainer.py ^
+    --n 5000 10000 5000 ^
+    --k 100 200 100 ^
+    --data_dir ../../data ^
+    -e ../../data/embedding/glove.840B.300d.gensim ^
+    --gpus 0 --use_amp
