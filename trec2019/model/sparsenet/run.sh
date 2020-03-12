4a5f824d27a9446e2bbeae87828888529b378c51
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-03-12 14:09:52 +0900

    trivial

--- trec2019/model/sparsenet/run.sh
+++ trec2019/model/sparsenet/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-python sparsenet_trainer.py --data_dir ../../data -e ../../data/embedding/glove.840B.300d.gensim --fast_dev_run --dense disc --profile 
+python sparsenet_trainer.py --data_dir ../../data --fast_dev_run --dense bert --gpus -1 -d ddp --n 2000 10000 2000 --k 40 200 40
