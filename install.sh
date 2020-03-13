2b589b4ce00eef674410c7b014bd62872d01ab8b
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-03-13 21:40:14 +0900

    trivial

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-pip install -e . && pip install -r requirements.txt && pip install -r requirements-dev.txt
+pip install -e . && pip install -r requirements.txt && pip install -r dev-requirements.txt
