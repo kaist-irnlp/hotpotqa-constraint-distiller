c1e509d9eef7a4b4e68ce3c3af69dbe266ed5d9b
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-03-21 15:16:14 +0900

    trivial

--- trec2019/model/sparsenet/sparsenet.py
+++ trec2019/model/sparsenet/sparsenet.py
@@ -32,6 +32,7 @@ from transformers import BertTokenizer
 import gensim
 from gensim.models.keyedvectors import KeyedVectors
 import numpy as np
+import pandas as pd
 
 from trec2019.utils.dataset import *
 from trec2019.model.sparsenet.helper import *
@@ -44,6 +45,7 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+root_dir = str(Path(__file__).parent.absolute())
 
 
 class SparseNet(pl.LightningModule):
@@ -51,12 +53,47 @@ class SparseNet(pl.LightningModule):
         super(SparseNet, self).__init__()
         self.hparams = hparams
         self.encoded = None
-        self.dense = dense
-        self.vocab = vocab
 
         # network
+        self._init_dense()
         self._preprocess_sparse_params()
         self._init_sparse()
+
+    def _get_dataset_cls(self):
+        return {"bow": BowDataset, "bert": BERTDataset}[self.hparams.dense]
+
+    def prepare_data(self):
+        # data_dir = Path(self.hparams.data_dir)
+        data_path = self.hparams.data_path
+        train, val, test = self.get_train_val_test(data_path)
+        dataset_cls = self._get_dataset_cls()
+        self._train_dataset = dataset_cls(train, self.vocab)
+        self._val_dataset = dataset_cls(val, self.vocab)
+        self._test_dataset = dataset_cls(test, self.vocab)
+
+    def _get_dense_vocab(self):
+        VOCAB_PATH = Path(root_dir) / "../../vocab/vocab.parquet"
+        VECTORS = "fasttext.en.300d"
+        MIN_FREQ = 2
+        vocab_counts = Counter(
+            {row.word: row.count for row in pd.read_parquet(VOCAB_PATH).itertuples()}
+        )
+        return Vocab(vocab_counts, vectors=VECTORS, min_freq=MIN_FREQ)
+
+    def _get_dense_bow(self, vectors):
+        return BowEmbedding(vectors)
+
+    def _get_dense_bert(self, weights):
+        return BertEmbedding(weights)
+
+    def _init_dense(self):
+        # init vocab
+        if self.hparams.dense == "bow":
+            self.dense_vocab = self._get_dense_vocab()
+            self.dense = self._get_dense_bow(self.dense_vocab.vectors)
+        elif self.hparams.dense == "bert":
+            self.dense_weights = "bert-base-uncased"
+            self.dense = self._get_dense_bert(self.dense_weights)
 
     def distance(self, a, b):
         # return torch.pow(a - b, 2).sum(1).sqrt()
@@ -360,14 +397,6 @@ class SparseNet(pl.LightningModule):
         train, val_test = train_test_split(indices, test_size=0.2)
         val, test = train_test_split(val_test, test_size=0.5)
         return dataset.oindex[train, :], dataset.oindex[val, :], dataset.oindex[test, :]
-
-    def prepare_data(self):
-        # data_dir = Path(self.hparams.data_dir)
-        data_path = self.hparams.data_path
-        train, val, test = self.get_train_val_test(data_path)
-        self._train_dataset = TripleDataset(train, self.vocab)
-        self._val_dataset = TripleDataset(val, self.vocab)
-        self._test_dataset = TripleDataset(test, self.vocab)
 
     def configure_optimizers(self):
         # can return multiple optimizers and learning_rate schedulers
