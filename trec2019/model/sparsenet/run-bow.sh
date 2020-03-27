46bdaa2f34fdc22bdd11356517b82c8161e5cfcf
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-03-27 12:26:30 +0900

    run-bow argument

--- trec2019/model/sparsenet/run-bow.sh
+++ trec2019/model/sparsenet/run-bow.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
 DATA_DIR=$1
-N="1000 3000 5000 10000"
-K="50 100 200 300 500"
+GPU=$2
+N=$3
+K=$4
 
 for n in $N
 do
     for k in $K
     do
-        python sparsenet_trainer.py --data_dir ${DATA_DIR} --gpus 0 --n ${n} --k ${k}
+        python sparsenet_trainer.py --data_dir ${DATA_DIR} --gpus ${GPU} --n ${n} --k ${k}
     done
 done
