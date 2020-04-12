7b93e051ffc2b0d6a882be0f7fd529c89e68f56d
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-04-12 17:05:19 +0900

    trivial

--- trec2019/model/sparsenet/sparsenet_trainer.py
+++ trec2019/model/sparsenet/sparsenet_trainer.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_dir", type=str, default=None, required=True)
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--epochs", dest="max_nb_epochs", default=500, type=int)
-    parser.add_argument("--learning_rate", "-lr", default=0.0001, type=float)
+    parser.add_argument("--learning_rate", "-lr", default=0.0002, type=float)
 
     # add default & model params
 
