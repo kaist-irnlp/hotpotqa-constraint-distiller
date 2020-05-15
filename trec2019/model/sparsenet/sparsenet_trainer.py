7d8b02e801c8de0634fd63117d658d1a4bfa1c91
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-05-15 23:17:41 +0900

    trivial

--- trec2019/model/sparsenet/sparsenet_trainer.py
+++ trec2019/model/sparsenet/sparsenet_trainer.py
@@ -79,6 +79,7 @@ def main(hparams):
     # flatten hparams
     log_params = {}
     for vals in vars(hparams)["content"].values():
+        print(dir(vals))
         log_params.update(dict(vals))
     # init logger
     neptune_logger = NeptuneLogger(
