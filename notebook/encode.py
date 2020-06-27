03444804f6160ae94769c122022346823f829155
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-06-28 00:52:51 +0900

    modify encoder

--- notebook/encode.py
+++ notebook/encode.py
@@ -11,6 +11,7 @@ import logging
 from pathlib import Path
 import zarr
 import torch
+from torch.utils.data import Dataset, DataLoader
 from transformers import AutoModel, AutoConfig, AutoTokenizer
 
 torch.set_grad_enabled(False)
@@ -20,6 +21,8 @@ logging.basicConfig(level=logging.INFO)
 parser = ArgumentParser()
 parser.add_argument("fpath", type=str)
 parser.add_argument("gpu", type=int, default=0)
+parser.add_argument("--batch_size", type=int, default=4)
+parser.add_argument("--emb_dim", type=int, default=768)
 parser.add_argument("--start", type=int, default=0)
 parser.add_argument("--end", type=int, default=None)
 args = parser.parse_args()
@@ -75,40 +78,41 @@ output_dir = Path("./output")
 if not output_dir.exists():
     output_dir.mkdir(parents=True)
 
+
+class TextDataset(Dataset):
+    def __init__(self, data_path):
+        self.data = zarr.open(str(data_path)).text[:]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+
+# load data
 fpath = Path(args.fpath)
-z = zarr.open(str(fpath))
-docs = z.text[args.start : args.end][:]
-real_shape = z.text.shape
-num_digits = real_shape[0] // 10
+dset = TextDataset(fpath)
+loader = DataLoader(dset, batch_size=args.batch_size, num_workers=0)
 
 # save to
 emb_path = "dense/bert"
-emb_dim = 768
+emb_dim = args.emb_dim
+z = zarr.open(str(fpath))
 if emb_path not in z:
-    z.zeros("dense/bert", shape=(len(z.text), emb_dim), chunks=(64, None), dtype="f4")
-z_embs = z[emb_path]
-
-
-# In[6]:
-
-CHUNK_SIZE = 8
-i = 0
-num_chunks = int(len(docs) / CHUNK_SIZE)
-print("# of chunks", num_chunks)
-while True:
-    print(f"{i} / {num_chunks}")
-    start, end = i * CHUNK_SIZE, (i + 1) * CHUNK_SIZE
-    real_start, real_end = start + args.start, end + args.start
-    embs = batch_encode(docs[start:end])
-    # save
-    embs = embs.cpu()
-    z_embs[real_start:real_end] = embs[:]
-    # embs_list.append(embs)
-    np.save(
-        output_dir / f"{real_start:07d}_{(real_start + embs.shape[0]):07d}.npy", embs,
+    z_embs = z.zeros(
+        "dense/bert", shape=(len(z.text), emb_dim), chunks=(64, None), dtype="f4"
     )
-    # check exit condition
-    if embs.shape[0] < CHUNK_SIZE:
-        break
-    i += 1
-# embs = torch.cat(embs_list, dim=0).cpu().numpy()
+else:
+    z_embs = z[emb_path]
+
+
+# encode & save
+for i, batch in enumerate(tqdm(loader)):
+    # encode
+    embs = batch_encode(batch).cpu()
+    # save
+    start = i * args.batch_size
+    end = start + embs.shape[0]
+    z_embs[start:end] = embs[:]
+
