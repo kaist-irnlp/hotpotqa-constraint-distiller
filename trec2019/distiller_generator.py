04c2ccb6337df6bf8d62ca8428315ff7b0f575f5
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-07-03 00:23:40 +0900

    trivial

--- trec2019/distiller_generator.py
+++ trec2019/distiller_generator.py
@@ -5,35 +5,39 @@ from argparse import ArgumentParser
 from argparse import Namespace
 import yaml
 import zarr
+import numpy as np
 from tqdm import tqdm
 
 from trec2019.distiller import Distiller
 from trec2019.utils.dataset import EmbeddingDataset
 
 parser = ArgumentParser()
-parser.add_argument("ckpt_dir", type=str)
-parser.add_argument("dataset_path", type=str)
+parser.add_argument("exp_dir", type=str)
+parser.add_argument("dataset_dir", type=str)
 parser.add_argument("--gpu", type=int, default=0)
-parser.add_argument("--use_last", action="store_true")
 parser.add_argument("--batch_size", type=int, default=8192)
 parser.add_argument("--num_workers", type=int, default=4)
 args = parser.parse_args()
 
 
-def get_model_path(ckpt_dir, use_last):
+def get_model_path(ckpt_dir, use_last=False):
     ckpt_files = ckpt_dir.glob("*.ckpt")
-    if use_last:
-        f = [f for f in ckpt_files if f.name.startswith("last_")][0]
-    else:
-        f = [f for f in ckpt_files if not f.name.startswith("last_")][0]
-    return str(f)
+    try:
+        if use_last:
+            f = [f for f in ckpt_files if f.name.startswith("last_")][0]
+        else:
+            f = [f for f in ckpt_files if not f.name.startswith("last_")][0]
+
+        return str(f)
+    except:
+        return None
 
 
-def override_hparams(hparams_path, dataset_path):
+def override_hparams(hparams_path, dataset_dir):
     with open(hparams_path, encoding="utf-8") as fp:
         y = yaml.load(fp, Loader=yaml.SafeLoader)
     # modify and save
-    y["dataset"]["path"] = dataset_path
+    y["dataset"]["path"] = dataset_dir
     y["dataset"]["on_memory"] = False
     with open(hparams_path, "w", encoding="utf-8") as fp:
         yaml.dump(y, fp)
@@ -72,7 +76,7 @@ def get_model_properties(hparams):
     return "_".join([str(p) for p in properties])
 
 
-def encode_dset(dset_path, emb_path):
+def encode_dset(model, hparams, dset_path, emb_path):
     dataset = EmbeddingDataset(dset_path, emb_path)
     loader = DataLoader(
         dataset,
@@ -80,6 +84,9 @@ def encode_dset(dset_path, emb_path):
         num_workers=args.num_workers,
         pin_memory=True,
     )
+    # model props
+    model_props = get_model_properties(hparams)
+    print(model_props)
     # encode
     z = zarr.open(str(dset_path))
     out_shape = (len(z.id), int(hparams["model"]["n"][-1]))
@@ -94,15 +101,33 @@ def encode_dset(dset_path, emb_path):
         start = args.batch_size * i
         end = start + out.shape[0]
         z_out[start:end] = out[:]
+        # sparsity
+        # n_filled = len(out.nonzero()[0])
+        # print("AVG sparsity", n_filled / out.size)
 
 
-if __name__ == "__main__":
+def main(ckpt_dir, dataset_dir):
+    exp_id = ckpt_dir.parent.parent.name
+    print("Processing", exp_id)
     # get paths
-    ckpt_dir = Path(args.ckpt_dir)
-    model_path = get_model_path(ckpt_dir, args.use_last)
-    hparams_path = str(ckpt_dir / "hparams.yaml")
-    override_hparams(hparams_path, args.dataset_path)
-    # hparam_overrides = {"dataset": {"path": args.dataset_path}}
+    ckpt_dir = Path(ckpt_dir)
+    model_path = get_model_path(ckpt_dir)
+    hparams_path = ckpt_dir / "hparams.yaml"
+    if (model_path is None) or (not hparams_path.exists()):
+        print(f"checkpoint or hparams does not exist: {exp_id}")
+        return None
+    hparams_path = str(hparams_path)
+    hparams = load_hparams(hparams_path)
+
+    # skip if SparseNet
+    if hparams["model"]["name"] == "sparsenet":
+        print(f"Skipping SparseNet: {exp_id}")
+        return None
+
+    model_props = get_model_properties(hparams)
+    # check if ckpt exists
+    override_hparams(hparams_path, dataset_dir)
+    # hparam_overrides = {"dataset": {"path": dataset_dir}}
 
     # load model
     device = f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu"
@@ -116,12 +141,15 @@ if __name__ == "__main__":
     model.freeze()
 
     # load hparams & parse model properties
-    hparams = load_hparams(hparams_path)
-    model_props = get_model_properties(hparams)
     emb_path = hparams["dataset"]["emb_path"]
-    print(model_props)
 
     # encode: queries
-    args.dataset_path = "E:/Data/msmarco-passages"
-    encode_dset(Path(args.dataset_path) / "queries.eval.zarr", emb_path)
-    encode_dset(Path(args.dataset_path) / "docs.eval.zarr", emb_path)
+    # dataset_dir = "E:/Data/msmarco-passages"
+    encode_dset(model, hparams, Path(dataset_dir) / "queries.eval.zarr", emb_path)
+    encode_dset(model, hparams, Path(dataset_dir) / "docs.eval.zarr", emb_path)
+
+
+if __name__ == "__main__":
+    out_dirs = list(Path(args.exp_dir).glob("**/output"))
+    for out_dir in tqdm(out_dirs):
+        main((out_dir / "checkpoints"), args.dataset_dir)
