7ac5fab4f0184117f91cdafb364fc2969f2dd3ee
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-08-19 15:58:06 +0900

    SIF 완료

--- trec2019/utils/dense.py
+++ trec2019/utils/dense.py
@@ -1,23 +1,51 @@
+import logging
 from abc import ABC, abstractclassmethod, abstractmethod
-from textblob import TextBlob
-from gensim.models import KeyedVectors
+from argparse import ArgumentParser, Namespace
+from collections import defaultdict
+
+import gensim.downloader as api
 import numpy as np
 import torch.nn.functional as F
-import gensim.downloader as api
-from argparse import ArgumentParser
+from gensim.models import KeyedVectors
 from sklearn.preprocessing import normalize
-import logging
+
+from textblob import TextBlob
+from gensim.parsing.preprocessing import preprocess_string
+from gensim.parsing.preprocessing import (
+    remove_stopwords,
+    strip_punctuation,
+    strip_tags,
+    strip_short,
+    strip_numeric,
+    strip_multiple_whitespaces,
+    split_alphanum,
+    strip_non_alphanum,
+    stem_text,
+)
+from fse.models import SIF
+from fse import IndexedList
 
 logging.basicConfig(
     format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__file__)
 
+PREPROCESS_FILTERS = [
+    lambda x: x.lower(),
+    strip_tags,
+    strip_punctuation,
+    strip_multiple_whitespaces,
+    # strip_numeric,
+    remove_stopwords,
+    strip_short,
+    split_alphanum,
+    strip_non_alphanum,
+]
+
 
 class AbstractDense(ABC):
     def __init__(self, args):
         self.args = args
-        self.__dim = None
         self._init()
 
     @abstractmethod
@@ -32,9 +60,38 @@ class AbstractDense(ABC):
     def encode(self, text):
         pass
 
+    @abstractmethod
+    def encode_batch(self, texts):
+        pass
+
     @property
     def dim(self):
-        return self.__dim
+        return self._dim
+
+
+class DenseSIF(AbstractDense):
+    def __init__(self, args):
+        super().__init__(args)
+
+    def _init(self):
+        self.model = SIF.load(self.args.model_path)
+        self._dim = self.model.wv.vector_size
+
+    def _tokenize(self, text):
+        return preprocess_string(text, filters=PREPROCESS_FILTERS)
+
+    def add_argparse_args(parser):
+        parser.add_argument("--model_path", type=str, required=True)
+        return parser
+
+    def encode(self, text):
+        sent = IndexedList([self._tokenize(text)])
+        return self.model.infer(sent).squeeze()
+
+    def encode_batch(self, texts):
+        tokens = [self._tokenize(txt) for txt in texts]
+        sents = IndexedList(tokens)
+        return self.model.infer(sents)
 
 
 class DenseDisc(AbstractDense):
@@ -43,62 +100,62 @@ class DenseDisc(AbstractDense):
 
     def _init(self):
         self.oov_vector = {}
-        self._init_embedding()
-
-    def _init_embedding(self):
-        model_choice = self.args.emb_model
+        model_choice = self.args.emb
         model_name = {
             "word2vec": "word2vec-google-news-300",
-            "glove": "glove-wiki-gigaword-300",
+            "glove": "glove-wiki-gigaword-50",
         }[model_choice]
         logger.info(f"Loading {model_name}...")
         self.model = api.load(model_name)
-        self.vector = normalize(self.model.vectors, axis=1)
-        self.vocab = self.model.vocab
-        self.__dim = self.vector.shape[1]
+        self.model.init_sims(replace=True)
+        self._dim = self.model.vector_size
 
     def add_argparse_args(parser):
         parser.add_argument(
-            "--emb_model", type=str, choices=("word2vec", "glove"), default="glove"
+            "--emb", type=str, choices=("word2vec", "glove"), default="glove"
         )
-        parser.add_argument("--ngram", type=int, default=3)
+        parser.add_argument("--ngram", type=int, default=2)
         return parser
 
-    def get_rand_vector(self):
-        v = np.random.rand(self.dim)
-        v = v / (v ** 2).sum() ** 0.5
-        return v
+    def _get_randn_vector(self):
+        v = np.random.randn(self.dim)
+        return self._normalize_vector(v)
 
-    def get_vector(self, token):
-        if token in self.vocab:
-            return self.vector[token]
+    def _normalize_vector(self, vec):
+        return vec / np.linalg.norm(vec)
+
+    def _get_vector(self, token):
+        if token in self.model.vocab:
+            return self.model.get_vector(token)
         else:
             if token not in self.oov_vector:
-                self.oov_vector[token] = self.get_rand_vector()
+                self.oov_vector[token] = self._get_randn_vector()
             return self.oov_vector[token]
 
     def encode(self, text):
-        vector_parts = []
         blob = TextBlob(text)
-        for n in range(1, self.args.ngram + 1):
-            vec_part = np.zeros(self.dim)
-            i = 0
-            for i, ng in enumerate(blob.ngrams(n=n)):
-                vec_ng = np.ones(self.dim)
+        ngram = self.args.ngram
+        out_parts = []
+        for n in range(1, ngram + 1):
+            ng_vec_all = []
+            # collect all ngram vectors
+            for ng in blob.ngrams(n=n):
+                ng_vec = np.ones(self.model.vector_size)
                 for tok in ng:
-                    vec_ng *= self.get_vector(tok)
-                vec_part += vec_ng
-            vector_parts.append(vec_part / (i + 1))
-
-        return np.concatenate(vector_parts, axis=None)
+                    ng_vec *= self._get_vector(tok)
+                ng_vec_all.append(self._normalize_vector(ng_vec))
+            # normalize ngram vectors
+            ng_vec_all = self._normalize_vector(np.sum(ng_vec_all, axis=0))
+            out_parts.append(ng_vec_all)
+        return np.concatenate(out_parts, axis=None)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser = DenseDisc.add_argparse_args(parser)
+    parser = DenseSIF.add_argparse_args(parser)
     args = parser.parse_args()
     # encoder
-    encoder = DenseDisc(args)
-    encoded = encoder.encode("this is a good day")
+    encoder = DenseSIF(args)
+    encoded = encoder.encode_batch(["this is a good day"] * 3)
     print(encoded)
     print(encoded.shape)
